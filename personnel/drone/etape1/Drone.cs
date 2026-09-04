130a7e49a8d45c53231523f2ad723126801d2e22
Bixente Pouradier <pt70xqw@eduvaud.ch>
2026-09-04 11:15:53 +0200

    feat(drone): ajouter des objets
    [5][Done]

--- personnel/drone/etape1/Drone.cs
+++ personnel/drone/etape1/Drone.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +9,46 @@ namespace etape1
 {
     internal class Drone
     {
+        private int _posX;
+        private int _posY;
+        private int _battery;
 
+        public Drone(int posX, int posY, int battery)
+        {
+            this._posX = posX;
+            this._posY = posY;
+            this._battery = battery;
+        }
+
+        public Drone()
+        {
+            this._posX = 0;
+            this._posY = 0;
+            this._battery = 40;
+        }
+
+
+        public void change()
+        {
+            _battery =- 2;
+            _posX =+ 1;
+        }
+
+        public void draw()
+        {
+            Console.Clear();
+            if (_battery > 0)
+            {
+                Console.SetCursorPosition(_posX, _posY);
+                Console.WriteLine("x-o-x");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.SetCursorPosition(_posX, _posY);
+                Console.WriteLine("_____");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+        }
     }
 }
--- personnel/drone/etape1/Program.cs
+++ personnel/drone/etape1/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Security.Policy;
 using System.Text;
 using System.Threading;
@@ -12,55 +13,11 @@ namespace etape1
     {
         static void Main(string[] args)
         {
-            int posY = 0;
-            int posX = 0;
-            int batterie = 50;
-            int batterie2 = 50;
-            int batterie3 = 50;
+            Drone drone = new Drone();
 
-            while (batterie >= 2)
-            {
-                for(posY = 0; posY <= 20; posY += 10)
-                {
-                    dessinDrone(posX, posY);
-                }
-                posX++;
-                batterie = etatDrone(batterie);
-                batterie2 = etatDrone(batterie2);
-                batterie3 = etatDrone(batterie3);
-                Thread.Sleep(150);
-                Console.Clear();
-            }
-
-            for (posY = 0; posY <= 20; posY += 10)
-            {
-                dessinDroneMort(posX, posY);
-            }
-        }
-
-        static void dessinDrone (int posX, int POSY)
-        {
-            string drone = "x-o-x";
-
-
-            Console.SetCursorPosition(posX, POSY);
-            Console.WriteLine(drone);
-        }
-
-        static int etatDrone(int batterie)
-        {
-            batterie -= 2;
-            return batterie;
-        }
-
-        static void dessinDroneMort(int posX, int POSY)
-        {
-            string droneDead = "____";
-
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.SetCursorPosition(posX, POSY);
-            Console.WriteLine(droneDead);
-            Console.ForegroundColor = ConsoleColor.White;
+            drone.change();
+            drone.draw();
+            Thread.Sleep(1000);
         }
     }
 }
