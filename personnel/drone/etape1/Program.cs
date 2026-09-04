27cacb121a0d56a8dd89a2467f48250be52388b3
Bixente Pouradier <pt70xqw@eduvaud.ch>
2026-09-04 10:49:04 +0200

    feat(drone): recuperrer ce qui a été fait la semaine dernière
    [5][Done]

---
+++ personnel/drone/etape1/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace etape1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int posY = 0;
+            int posX = 0;
+            int batterie = 50;
+            int batterie2 = 50;
+            int batterie3 = 50;
+
+            while (batterie >= 2)
+            {
+                for(posY = 0; posY <= 20; posY += 10)
+                {
+                    dessinDrone(posX, posY);
+                }
+                posX++;
+                batterie = etatDrone(batterie);
+                batterie2 = etatDrone(batterie2);
+                batterie3 = etatDrone(batterie3);
+                Thread.Sleep(150);
+                Console.Clear();
+            }
+
+            for (posY = 0; posY <= 20; posY += 10)
+            {
+                dessinDroneMort(posX, posY);
+            }
+        }
+
+        static void dessinDrone (int posX, int POSY)
+        {
+            string drone = "x-o-x";
+
+
+            Console.SetCursorPosition(posX, POSY);
+            Console.WriteLine(drone);
+        }
+
+        static int etatDrone(int batterie)
+        {
+            batterie -= 2;
+            return batterie;
+        }
+
+        static void dessinDroneMort(int posX, int POSY)
+        {
+            string droneDead = "____";
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.SetCursorPosition(posX, POSY);
+            Console.WriteLine(droneDead);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
---
+++ personnel/drone/etape1/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Les informations générales relatives à un assembly dépendent de
+// l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
+// associées à un assembly.
+[assembly: AssemblyTitle("etape1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("DGEP")]
+[assembly: AssemblyProduct("etape1")]
+[assembly: AssemblyCopyright("Copyright © DGEP 2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// L'affectation de la valeur false à ComVisible rend les types invisibles dans cet assembly
+// aux composants COM. Si vous devez accéder à un type dans cet assembly à partir de
+// COM, affectez la valeur true à l'attribut ComVisible sur ce type.
+[assembly: ComVisible(false)]
+
+// Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
+[assembly: Guid("257c7ff2-858f-4f3e-bc77-9bb4138c9f18")]
+
+// Les informations de version pour un assembly se composent des quatre valeurs suivantes :
+//
+//      Version principale
+//      Version secondaire
+//      Numéro de build
+//      Révision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
