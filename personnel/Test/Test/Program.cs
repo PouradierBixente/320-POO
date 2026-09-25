f983e094db4eb8adebad307fe9358e7c8e6d0456
Bixente Pouradier <pt70xqw@eduvaud.ch>
2026-09-25 11:19:20 +0200

    feat(test): cree un programme pour découvrir comment fonctionne les tests

---
+++ personnel/Test/Test/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+namespace Test
+{
+    // See https://aka.ms/new-console-template for more information
+    Console.WriteLine("Hello, World!");
+    Console.ReadKey();
+
+    class MyMath
+        {
+            public static int Somme(int a, int b)
+            {
+                return a + b;
+            }
+        }
+}
---
+++ personnel/Test/Test_My_Test/Test1.cs
@@ -0,0 +1,26 @@
+﻿using Test
+  
+
+namespace Test_My_Test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestLaSommeDixEtDixEgalVingt()
+        {
+            //Arrange
+            int x = 10;
+            int y = 10;
+            int z = -15;
+
+            //Act
+            int res = MyMath.Somme(x, y);
+            int res2 = MyMath.Somme(x, z);
+
+            //Assert
+            Assert.AreEqual(20, res);
+            Assert.AreEqual(-5, res2);
+        }
+    }
+}
